8dedaa1d9c6b6e191d65af4bbfab2080c3a04893
ejcho <ejcho.dev@gmail.com>
2022-06-09 00:27:13 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,9 +6,18 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
-public class Address {
+public class Address { // 값 타입은 변경 불가능하게 설계해야 한다
 
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
